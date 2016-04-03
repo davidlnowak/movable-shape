59446d4b5ce1029af098a5c09841c3af78d328f1
davidlnowak <nowak.david@gmail.com>
2016-04-03 12:59:40 -0400

    Complete Working Application
    All coding complete for this application.

--- sketch.js
+++ sketch.js
@@ -1,22 +1,44 @@
 // give movableShape a nice, descriptive name to work with here
 // (i.e., replace the variable name myShape with something more descriptive (e.g., myCar, myRocket).)
-var myShape = movableShape;
+var movableRocket = movableShape;
 
 // Redefine drawShape() by putting your moving shape in here
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
-myShape.drawShape = function () {
-	// your code goes here
-};
+movableRocket.drawShape = function () {
+	// Rocket nose
+  fill('#DF3A01');
+  triangle(300, 50, 240, 150, 360, 150);
 
-// You will also need to do some work to set the speed of the shape. How do you want it to move? What do you need to change to get it to move?
+  // Rocket body
+  fill('#D8D8D8');
+  rect(240, 150, 120, 350);
+
+  // Window
+  fill('#030A6B');
+  ellipse(300, 225, 60, 60);
+
+  fill('#81BEF7');
+  ellipse(300, 225, 48, 48);
+
+  // Fins
+  fill('#DF3A01');
+  quad(240, 400, 175, 460, 175, 500, 240, 475);
+  quad(360, 400, 425, 460, 425, 500, 360, 475);
+
+  // Thruster
+  fill('#2E2E2E');
+  quad(250, 500, 245, 517, 355, 517, 350, 500);
+};
 
 setup = function() {
 	createCanvas(600, 600);
+
+	movableRocket.speed.y = -1;
 };
 
 draw = function() {
   background(100); // refresh the background
-  myShape.display(); // display myShape
-  myShape.update(); // and then update it
+  movableRocket.display(); // display myShape
+  movableRocket.update(); // and then update it
 };
